04a458a79f6d408589c0e7819a2cb84dca77d82d
Freddy <freddy.artea@gmail.com>
2020-10-17 15:55:30 -0500

    boton flotante FloatingAction Button

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -22,6 +22,12 @@ class HomePage extends StatelessWidget {
           Text('0', style: estiloTexto),
         ],
       )),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          print('Hola Mundo!');
+        },
+      ),
     );
   }
 }
