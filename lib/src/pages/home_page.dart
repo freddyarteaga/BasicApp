df4063c0350bb541f1c92b1e8293ae9f25d6e454
Freddy <freddy.artea@gmail.com>
2020-10-17 15:23:34 -0500

    Widget Scaffold

---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Titulo'),
+        centerTitle: true,
+      ),
+      body: Center(child: Text('Hola Mundo')),
+    );
+  }
+}
